a49653c04b6673ee5eddbee456f5de4581e1cce4
edsim97 <edsim97@yandex.ru>
2024-07-23 01:34:41 +0300

    Мелкие правки

--- hw02-annotation-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Repository
