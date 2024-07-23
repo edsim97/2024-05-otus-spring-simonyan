479aba51c3f99f83868d13da46c57ed18f9b0151
edsim97 <edsim97@yandex.ru>
2024-07-23 16:25:34 +0300

    Переезд на Shell

--- hw04-spring-shell/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw04-spring-shell/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.otus.hw.security.LoginContext;
 
 @Service
 @RequiredArgsConstructor
@@ -9,13 +10,13 @@ public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final TestService testService;
 
-    private final StudentService studentService;
-
     private final ResultService resultService;
+
+    private final LoginContext loginContext;
 
     @Override
     public void run() {
-        var student = studentService.determineCurrentStudent();
+        var student = loginContext.getCurrentStudent();
         var testResult = testService.executeTestFor(student);
         resultService.showResult(testResult);
     }
---
+++ hw04-spring-shell/src/main/java/ru/otus/hw/shell/TestCommands.java
@@ -0,0 +1,44 @@
+package ru.otus.hw.shell;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.shell.Availability;
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.command.annotation.CommandAvailability;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+import org.springframework.shell.standard.ShellOption;
+import ru.otus.hw.security.LoginContext;
+import ru.otus.hw.service.LocalizedIOService;
+import ru.otus.hw.service.TestRunnerService;
+
+@Command(group = "Test app commands")
+@RequiredArgsConstructor
+public class TestCommands {
+
+    private final LocalizedIOService ioService;
+
+    private final LoginContext loginContext;
+
+    private final TestRunnerService testRunnerService;
+
+    @Command(description = "Set first name and last name of current student", command = "login", alias = "l")
+    public String login(@ShellOption String firstName, @ShellOption String lastName) {
+
+        this.loginContext.login(firstName, lastName);
+        return this.ioService.getMessage("TestCommands.login.success", firstName, lastName);
+    }
+
+    @Command(description = "Starts student testing", command = "start", alias = {"s", "run"})
+    @CommandAvailability(provider = "startCommandAvailabilityProvider")
+    public void start() {
+
+        this.testRunnerService.run();
+    }
+
+    @Command(description = "Exit", command = "exit", alias = "q")
+    public void exit() {
+
+
+    }
+}
