300383f59dddf490975f7e2766babcbc6dc2f45f
edsim97 <edsim97@yandex.ru>
2024-10-14 23:32:05 +0300

    Сделал репы

--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,33 +1,48 @@
 package ru.otus.hw.repositories;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcAuthorRepository implements AuthorRepository {
+
+    private final NamedParameterJdbcOperations jdbc;
 
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+
+        return this.jdbc.query("select id, full_name from authors", new AuthorRowMapper());
     }
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+
+        Map<String, Object> params = Collections.singletonMap("id", id);
+
+        return Optional.ofNullable(this.jdbc.queryForObject(
+            "select id, full_name from authors where id = :id",
+            params,
+            new AuthorRowMapper()
+        ));
     }
 
-    private static class AuthorRowMapper implements RowMapper<Author> {
+    public static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
         public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+
+            return new Author(rs.getLong("id"), rs.getString("full_name"));
         }
     }
 }
