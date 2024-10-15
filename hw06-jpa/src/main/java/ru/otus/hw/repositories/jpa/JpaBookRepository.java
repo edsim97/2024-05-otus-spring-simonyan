9ddb8c40d32c533bb79c96f4ac1e7b101121327c
edsim97 <edsim97@yandex.ru>
2024-10-16 00:43:32 +0300

    JPA репы

---
+++ hw06-jpa/src/main/java/ru/otus/hw/repositories/jpa/JpaBookRepository.java
@@ -0,0 +1,48 @@
+package ru.otus.hw.repositories.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.repositories.BookRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class JpaBookRepository implements BookRepository {
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    @Override
+    public Optional<Book> findById(long id) {
+
+        return Optional.ofNullable(em.find(Book.class, id));
+    }
+
+    @Override
+    public List<Book> findAll() {
+
+        return em.createQuery("select b from books b", Book.class).getResultList();
+    }
+
+    @Override
+    public Book save(Book book) {
+
+        if (book.getId() == 0) {
+
+            em.persist(book);
+            return book;
+        }
+        return em.merge(book);
+    }
+
+    @Override
+    public void deleteById(long id) {
+
+        em.remove(em.find(Book.class, id));
+    }
+}
