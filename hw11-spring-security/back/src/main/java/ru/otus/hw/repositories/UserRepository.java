ba37aa8908b4c978cff87884152b4022c730432a
edsim97 <edsim97@yandex.ru>
2024-11-05 21:13:06 +0300

    Правка репы

--- hw11-spring-security/back/src/main/java/ru/otus/hw/repositories/UserRepository.java
+++ hw11-spring-security/back/src/main/java/ru/otus/hw/repositories/UserRepository.java
@@ -1,6 +1,19 @@
 package ru.otus.hw.repositories;
 
+import jakarta.annotation.Nonnull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.hw.models.User;
 
-public interface UserRepository extends JpaRepository<User, Long> { }
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = "roles")
+    Optional<User> findById(long id);
+
+    @EntityGraph(attributePaths = "roles")
+    @Nonnull
+    List<User> findAll();
+}
