3c647006856647b8a8f26cde3fed71bd327a3dad
edsim97 <edsim97@yandex.ru>
2024-11-08 03:25:17 +0300

    EAGER -> LAZY

--- hw11-spring-security/src/main/java/ru/otus/hw/models/Role.java
+++ hw11-spring-security/src/main/java/ru/otus/hw/models/Role.java
@@ -2,6 +2,7 @@ package ru.otus.hw.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,7 +23,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     @Column(name = "name")
