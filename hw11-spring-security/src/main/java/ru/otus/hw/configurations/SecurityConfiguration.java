a7dcc71ac16527a769b3df2aeb70fb817fa79549
edsim97 <edsim97@yandex.ru>
2024-11-20 23:46:34 +0300

    Правка энкодера

--- hw11-spring-security/src/main/java/ru/otus/hw/configurations/SecurityConfiguration.java
+++ hw11-spring-security/src/main/java/ru/otus/hw/configurations/SecurityConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -45,6 +45,6 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
 
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
