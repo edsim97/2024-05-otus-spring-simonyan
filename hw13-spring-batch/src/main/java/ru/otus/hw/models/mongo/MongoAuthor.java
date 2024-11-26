d478483df5a190ecc0a2299cedbed80e99c11815
edsim97 <edsim97@yandex.ru>
2024-11-26 21:56:38 +0300

    Дело сделано

--- hw13-spring-batch/src/main/java/ru/otus/hw/models/mongo/MongoAuthor.java
+++ hw13-spring-batch/src/main/java/ru/otus/hw/models/mongo/MongoAuthor.java
@@ -1,6 +1,7 @@
 package ru.otus.hw.models.mongo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -10,6 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Document(collection = "authors")
 public class MongoAuthor {
 
--- hw13-spring-batch/src/main/java/ru/otus/hw/repositories/mongo/MongoGenreRepository.java
+++
@@ -1,6 +0,0 @@
-package ru.otus.hw.repositories.mongo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import ru.otus.hw.models.mongo.MongoGenre;
-
-public interface MongoGenreRepository extends MongoRepository<MongoGenre, String> { }
